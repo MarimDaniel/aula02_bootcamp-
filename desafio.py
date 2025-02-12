340dfae3f06b6e71d0356728b06c98873b19a8ca
Daniel Marim <danielmarim@gmail.com>
2025-02-12 02:37:09 -0300

    Adicionando o desafio

---
+++ desafio.py
@@ -0,0 +1,13 @@
+try:
+    nome = input("Digite seu nome: ")
+
+    # Verifica se o nome está vazio
+    if len(nome) == 0:
+        raise ValueError("O nome não pode estar vazio.")
+    # Verifica se há números no nome
+    elif any(char.isdigit() for char in nome):
+        raise ValueError("O nome não deve conter números.")
+    else:
+        print("Nome válido:", nome)
+except ValueError as e:
+    print(e)
